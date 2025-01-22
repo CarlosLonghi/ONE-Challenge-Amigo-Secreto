d6151e69e05674d209625f450bff547121e525ce
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-01-22 17:37:46 -0300

    Refactor and add new validations

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ O usuário deverá adicionar nomes por meio de um campo de texto e de um botão 
 
 Os nomes inseridos devem ser exibidos em uma lista visível na página, e ao finalizar, um botão "Sortear Amigo" selecionará um dos nomes de forma aleatória, exibindo o resultado na tela.
 
-### Funcionalidades:
+## Funcionalidades:
 
 - [x] Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
 
--- app.js
+++ app.js
@@ -2,29 +2,35 @@
 
 let listaDeAmigos = [];
 let inputDeAmigos = document.getElementById('amigo');
+let sorteioRealizado = false;
 
 function adicionarAmigo() {
+  if (sorteioRealizado) {
+    reiniciarSorteio();
+  }
+
   let novoAmigo = inputDeAmigos.value;
 
   if (novoAmigo) {
     listaDeAmigos.push(novoAmigo);
     inputDeAmigos.value = '';
-    exibirListaDeAmigos('listaAmigos', listaDeAmigos);
+    exibirListaDeAmigos(listaDeAmigos);
   } else {
-    alert('Por favor, digite o nome de um amigo para adicioná-lo!');
+    alert('Por favor, insira um nome.');
   }
 }
 
-function exibirAmigoSorteado(idDoElemento, amigoSorteado) {
-  let elemento = document.getElementById(idDoElemento);
+function exibirAmigoSorteado(amigoSorteado) {
+  let elemento = document.getElementById('resultado');
   elemento.innerHTML = '';
   let li = document.createElement('li');
   li.textContent = `O amigo secreto sorteado é: ${amigoSorteado}`;
   elemento.appendChild(li);
 }
 
-function exibirListaDeAmigos(idDoElemento, listaDeAmigos) {
-  let elemento = document.getElementById(idDoElemento);
+// Dicidi adicionar os elementos individualmente na lista, de acordo com que são adicionados. Pois se fizesse um "for" como o descrito na tarefa do trello, para listar os elementos a cada nova adição, teria muita manipulação da DOM desnecessária.
+function exibirListaDeAmigos(listaDeAmigos) {
+  let elemento = document.getElementById('listaAmigos');
 
   let novoAmigo = listaDeAmigos[listaDeAmigos.length - 1];
   let li = document.createElement('li');
@@ -33,14 +39,30 @@ function exibirListaDeAmigos(idDoElemento, listaDeAmigos) {
 }
 
 function sortearAmigo() {
+  if (sorteioRealizado) {
+    alert('Adicione amigos novamente, para realizar um novo sorteio.');
+    return;
+  } else if (listaDeAmigos.length === 0) {
+    alert('Por favor, adicione amigos antes de sortear.');
+    return;
+  }
+
   let tamanhoDoArrayDeAmigos = listaDeAmigos.length;
   let indexDoAmigoSorteado = parseInt(Math.random() * tamanhoDoArrayDeAmigos);
 
+  sorteioRealizado = true;
+  exibirAmigoSorteado(listaDeAmigos[indexDoAmigoSorteado]);
   limparListaDeAmigos();
-  exibirAmigoSorteado('resultado', listaDeAmigos[indexDoAmigoSorteado]);
 }
 
 function limparListaDeAmigos() {
+  listaDeAmigos = [];
   let elemento = document.getElementById('listaAmigos');
+  elemento.innerHTML = '';
+}
+
+function reiniciarSorteio() {
+  sorteioRealizado = false;
+  let elemento = document.getElementById('resultado');
   elemento.innerHTML = '';
 }
