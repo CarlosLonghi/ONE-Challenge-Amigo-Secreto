97a2f6a1ccde6651053ab1d83a1c6197e9d920a8
CarlosLonghi <carloslonghi.cl@gmail.com>
2025-01-22 16:17:20 -0300

    Challenge Complete

--- README.md
+++ README.md
@@ -10,10 +10,10 @@ Os nomes inseridos devem ser exibidos em uma lista visível na página, e ao fin
 
 ### Funcionalidades:
 
-- Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
+- [x] Adicionar nomes: Os usuários escreverão o nome de um amigo em um campo de texto e o adicionarão a uma lista visível ao clicar em "Adicionar".
 
-- Validar entrada: Se o campo de texto estiver vazio, o programa exibirá um alerta solicitando um nome válido.
+- [x] Validar entrada: Se o campo de texto estiver vazio, o programa exibirá um alerta solicitando um nome válido.
 
-- Visualizar a lista: Os nomes inseridos aparecerão em uma lista abaixo do campo de entrada.
+- [x] Visualizar a lista: Os nomes inseridos aparecerão em uma lista abaixo do campo de entrada.
 
-- Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página.
+- [x] Sorteio aleatório: Ao clicar no botão "Sortear Amigo", um nome da lista será selecionado aleatoriamente e exibido na página.
--- app.js
+++ app.js
@@ -1,1 +1,46 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let listaDeAmigos = [];
+let inputDeAmigos = document.getElementById('amigo');
+
+function adicionarAmigo() {
+  let novoAmigo = inputDeAmigos.value;
+
+  if (novoAmigo) {
+    listaDeAmigos.push(novoAmigo);
+    inputDeAmigos.value = '';
+    exibirListaDeAmigos('listaAmigos', listaDeAmigos);
+  } else {
+    alert('Por favor, digite o nome de um amigo para adicioná-lo!');
+  }
+}
+
+function exibirAmigoSorteado(idDoElemento, amigoSorteado) {
+  let elemento = document.getElementById(idDoElemento);
+  elemento.innerHTML = '';
+  let li = document.createElement('li');
+  li.textContent = `O amigo secreto sorteado é: ${amigoSorteado}`;
+  elemento.appendChild(li);
+}
+
+function exibirListaDeAmigos(idDoElemento, listaDeAmigos) {
+  let elemento = document.getElementById(idDoElemento);
+
+  let novoAmigo = listaDeAmigos[listaDeAmigos.length - 1];
+  let li = document.createElement('li');
+  li.textContent = novoAmigo;
+  elemento.appendChild(li);
+}
+
+function sortearAmigo() {
+  let tamanhoDoArrayDeAmigos = listaDeAmigos.length;
+  let indexDoAmigoSorteado = parseInt(Math.random() * tamanhoDoArrayDeAmigos);
+
+  limparListaDeAmigos();
+  exibirAmigoSorteado('resultado', listaDeAmigos[indexDoAmigoSorteado]);
+}
+
+function limparListaDeAmigos() {
+  let elemento = document.getElementById('listaAmigos');
+  elemento.innerHTML = '';
+}
